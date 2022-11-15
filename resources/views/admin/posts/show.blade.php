93ed382d7d47c0ab1734e525f928e66f84fc578b
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2022-11-15 14:40:11 +0100

    PostController destroy

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -7,4 +7,12 @@
     <div class="mt-5">
         <a href="{{ route('admin.posts.edit', $post->id) }}">Edit</a>
     </div>
+
+    <div class="mt-2">
+        <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <input type="submit" value="Delete" onclick="confirm('Are you sure you want to delete this post?')">
+        </form>
+    </div>
 @endsection
